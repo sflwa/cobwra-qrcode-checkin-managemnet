c760183be8986e17f45f22eafcb6d3e0c980c75c
Philip Levine <philip@southfloridawebadvisors.com>
2026-02-19 06:30:20 -0500

    Update cobwra-checkin.php

--- cobwra-checkin.php
+++ cobwra-checkin.php
@@ -14,16 +14,19 @@ define( 'COBWRA_TEMP_FORM', 10 );
 define( 'COBWRA_FINAL_FORM', 6 );
 define( 'COBWRA_PATH', plugin_dir_path( __FILE__ ) );
 
+
 // Load Components
 require_once COBWRA_PATH . 'includes/class-cobwra-engine.php';
 require_once COBWRA_PATH . 'includes/class-cobwra-kiosk.php';
 require_once COBWRA_PATH . 'includes/class-cobwra-dashboard.php';
+require_once COBWRA_PATH . 'includes/class-cobwra-admin.php';
 
 final class COBWRA_Checkin_Plugin {
 	public function __construct() {
 		$engine = new COBWRA_Engine();
 		new COBWRA_Kiosk( $engine );
 		new COBWRA_Dashboard( $engine );
+		new COBWRA_Admin( $engine );
 	}
 }
 new COBWRA_Checkin_Plugin();
